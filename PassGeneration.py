ac29553ba4caa9b0ea7f8d9ac1d896c5445b7fa4
rouzbeh <rouzbeh.latifi@yahoo.com>
2022-02-12 16:20:31 +0330

    first commit

---
+++ PassGeneration.py
@@ -0,0 +1,19 @@
+import string,random
+
+while True:
+    length = int(input('please enter your password length: '))
+    charc = string.ascii_letters + string.digits + '!@#$%^&*()-+'
+    password = ''.join([random.choice(charc) for i in range(length)])
+
+    print('your pass is : {}'.format(password))
+
+
+    while True:
+        answer = input('Do you want another password (Y/N) : ').lower()
+
+        if answer == 'n' or answer == 'y':
+            break
+    
+    if answer == 'n':
+        break
+    print('a')
